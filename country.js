b434dd96832f42689f5eaaee397c6846ea965534
Chayathorn <aodzaza1130@gmail.com>
2020-04-23 21:18:04 +0700

    DAY4

--- country.js
+++ country.js
@@ -1,22 +1,28 @@
-$(function(){
-var urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams.get('country'));
-var country = urlParams.get('country');
-$("#country").html(country);
-var Total = 0;
-var url = "https://pomber.github.io/covid19/timeseries.json";
+$(function () {
+  var urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams.get("country"));
+  var country = urlParams.get("country");
+  $("#country").html(country);
+  var Total = 0;
+  var url = "https://pomber.github.io/covid19/timeseries.json";
 
-  $.getJSON(url, function(result) {
+  $.getJSON(url, function (result) {
     var selectedCountry = result[country];
     console.log(selectedCountry);
-    for(var i=0; i<selectedCountry.length; i++){
-        var row = `<tr>
+    for (var i = 0; i < selectedCountry.length; i++) {
+      var row = `<tr>
         <th scope="row">${selectedCountry[i].date}</th>
         <td>${selectedCountry[i].confirmed}</td>
         <td>${selectedCountry[i].deaths}</td>
         <td>${selectedCountry[i].recovered}</td>
                 </tr>`;
-        $("#data").append(row);
+      $("#data").append(row);
     }
+    var nowConfirmed = selectedCountry[selectedCountry.length - 1].confirmed;
+    var nowDeaths = selectedCountry[selectedCountry.length - 1].deaths;
+    var nowRecovered = selectedCountry[selectedCountry.length - 1].recovered;
+    $("#nowConfirmed").append(nowConfirmed);
+    $("#nowDeaths").append(nowDeaths);
+    $("#nowRecovered").append(nowRecovered);
   });
 });
